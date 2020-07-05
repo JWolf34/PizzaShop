1b0ae92f3d4605c380688f716c93ce634441d84d
BuildTools <unconfigured@null.spigotmc.org>
2020-07-05 18:42:36 -0500

    Abstracted Cheese.java and its subclasses.

--- src/shop/menu/pizza/topping/cheese/Cheese.java
+++ src/shop/menu/pizza/topping/cheese/Cheese.java
@@ -4,4 +4,26 @@ import shop.menu.pizza.topping.Topping;
 
 public abstract class Cheese extends Topping {
 
+    final String name;
+    final int calories;
+    final double price;
+
+    public Cheese(String name, int calories, double price){
+        this.name = name;
+        this.calories = calories;
+        this.price = price;
+    }
+
+    @Override
+    public String getItemName() {
+        return name;
+    }
+
+    @Override
+    public int getCalories() {
+        return calories;
+    }
+
+    @Override
+    public double getPrice() { return price; }
 }
--- src/shop/menu/pizza/topping/cheese/ParmesanCheese.java
+++ src/shop/menu/pizza/topping/cheese/ParmesanCheese.java
@@ -2,30 +2,7 @@ package shop.menu.pizza.topping.cheese;
 
 public class ParmesanCheese extends Cheese {
 
-    final String name;
-    final int calories;
-    final double price;
-
     public ParmesanCheese(){
-        this.name = "Parmesan Cheese";
-        this.calories = 130;
-        this.price = 0.0;
-    }
-
-    @Override
-    public String getItemName() {
-        return name;
-    }
-
-    @Override
-    public int getCalories() {
-        return calories;
-    }
-
-    @Override
-    public double getPrice() { return price; }
-
-    public String toString(){
-        return getItemName();
+        super("Parmesan Cheese", 130, 0.0);
     }
 }
